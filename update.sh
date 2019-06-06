258e987e41316dbc9359ce7ae0f1e16d86d4d045
Michael <freender.mail@gmail.com>
2019-06-05 22:14:21 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ git reset --hard origin/master
 git pull
 mv ~/docker-vm/docker-compose.yaml ~/docker-compose-letsencrypt-nginx-proxy-companion
 mv ~/docker-vm/update.sh ~/
+chmod +x ~/update.sh
 cd ~/docker-compose-letsencrypt-nginx-proxy-companion
 ~/docker-compose-letsencrypt-nginx-proxy-companion/start.sh
 docker-compose restart nginx-gen
