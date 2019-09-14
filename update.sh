dd231a2d6d7d0525236ad902cf409f7ea2cca672
Michael <freender.mail@gmail.com>
2019-09-14 13:01:19 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-. /home/freender/.profile
-echo $USER
 cd /home/freender/docker-vm
+echo "Removing old update script"
 rm *
+echo "Pulling new script from GitHub"
 git reset --hard origin/master
 git pull
 mv /home/freender/docker-vm/docker-compose.yaml /home/freender/docker-compose-letsencrypt-nginx-proxy-companion
 mv /home/freender/docker-vm/update.sh /home/freender/
 chmod +x /home/freender/update.sh
 cd /home/freender/docker-compose-letsencrypt-nginx-proxy-companion
+echo "Running docker-compose up -d"
 /home/freender/docker-compose-letsencrypt-nginx-proxy-companion/start.sh
 docker-compose restart nginx-gen
