d5f18df4490f588249cb035c7f566daa9ef5161c
Michael <freender.mail@gmail.com>
2019-09-14 12:22:24 -0400

    +cron support

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd ~/docker-vm
+cd /home/freender/docker-vm
 rm *
 git reset --hard origin/master
 git pull
-mv ~/docker-vm/docker-compose.yaml ~/docker-compose-letsencrypt-nginx-proxy-companion
-mv ~/docker-vm/update.sh ~/
-chmod +x ~/update.sh
-cd ~/docker-compose-letsencrypt-nginx-proxy-companion
-~/docker-compose-letsencrypt-nginx-proxy-companion/start.sh
+mv /home/freender/docker-vm/docker-compose.yaml /home/freender/docker-compose-letsencrypt-nginx-proxy-companion
+mv /home/freender/docker-vm/update.sh /home/freender/
+chmod +x /home/freender/update.sh
+cd /home/freender/docker-compose-letsencrypt-nginx-proxy-companion
+/home/freender/docker-compose-letsencrypt-nginx-proxy-companion/start.sh
 docker-compose restart nginx-gen
