84a9402ef1197f0afa845c86580eb792ae4e897e
Michael <freender.mail@gmail.com>
2020-02-10 19:07:01 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ rm *
 echo "Pulling new script from GitHub"
 git reset --hard origin/master
 git pull
-mv /home/freender/docker-vm/docker-compose.yaml /home/freender
-mv /home/freender/docker-vm/update.sh /home/freender
-chmod +x /home/freender/update.sh
+mv /home/freender/docker-vm/docker-compose.yaml /home/freender/docker-compose
+mv /home/freender/docker-vm/update.sh /home/freender/docker-compose
+chmod +x /home/freender/docker-compose/update.sh
 cd /home/freender
 echo "Running docker-compose up -d"
-/home/freender/start.sh
+/home/freender/docker-compose/start.sh
 docker-compose restart nginx-gen
 echo "Clean Up after Docker"
 docker system prune -f
