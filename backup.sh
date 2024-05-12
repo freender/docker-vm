2848dc59e83e256e350061d620c04d068faaaed1
freender <40869255+freender@users.noreply.github.com>
2024-05-12 14:57:21 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,12 +4,6 @@
 # crontab -e
 #0 9 * * 1,4 /home/pi/backup.sh > /home/pi/backup.txt 2>&1
 
-# DEPRECATED
-# Backup rsyslog config
-# sudo apt install rsyslog
-#cp /etc/rsyslog.conf /home/pi/backup/rsyslog.conf
-
-
 #Delete old backup files and create new backup
 now=$(date +"%m%d%Y")
 cd /home/pi/docker
@@ -21,19 +15,8 @@ docker compose stop
 # stop continers spawned by portainer
 docker stop $(docker ps -a -q)
 
-
-#Delete Nginx logs after backup
-#sudo zip -oqrm /home/pi/backup/nginx_logs_pi_$now.zip /home/pi/docker/nginx_proxy_manager/data/logs
-
-#Delete Certbot archived keys after backup
-#sudo zip -oqrm /home/pi/backup/nginx_keys_pi_$now.zip /home/pi/docker/nginx_proxy_manager/letsencrypt/csr /home/pi/docker/nginx_proxy_manager/letsencrypt/keys
-
-#Delete pihole logs after backup
-sudo zip -oqrm /home/pi/backup/pihole_logs_pi_$now.zip /home/pi/docker/pi-hole/etc-pihole/pihole-FTL.db
-
 #Backup docker appdata
 sudo zip -oqr /home/pi/backup/docker_backup_pi_$now.zip /home/pi/docker
-
 
 #Update portainer
 docker compose pull
