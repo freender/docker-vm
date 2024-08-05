5f0105a00c7743c33223e8939bfae6d8dc85312b
freender <40869255+freender@users.noreply.github.com>
2024-08-05 17:29:43 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -26,6 +26,9 @@ docker compose up -d
 #Start docker containers
 docker start $(docker ps -a -q -f status=exited)
 
+# Wait 2 minutes to start dockers
+wait 2m
+
 #Delete old backups
 sudo rm -rf `ls /home/pi/backup/pi-docker/ -t | awk 'NR>1'`
 
