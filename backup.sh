ee342aefb90a9e31f089844f0e38654b16295ede
freender <40869255+freender@users.noreply.github.com>
2025-03-22 14:38:25 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Add this script to cron
+# crontab -e
+#0 9 * * 1,4 /home/pi/backup.sh > /home/pi/backup.txt 2>&1
+
+# Define source and destination directories
+SRC="/home/pi/docker/"
+DEST="/home/pi/backup/pi-docker/"
+
+echo "===== Starting Docker Backup: $(date) ====="
+
+# Create new backup directory
+cd "$SRC"
+mkdir -p "$DEST"
+
+# stop all continers
+docker stop $(docker ps -a -q)
+
+#Backup docker appdata
+echo "Starting rsync backup..."
+rsync -avh --progress --delete "$SRC" "$DEST"
+
+#Start docker containers
+docker start $(docker ps -a -q -f status=exited)
+
+# Sleep 10 seconds to allow docker containers to start
+echo "Sleeping for 10 seconds to allow docker containers to start"
+sleep 10
+
+#Clean-up
+docker system prune -f
+
+echo "===== Backup Completed: $(date) ====="
